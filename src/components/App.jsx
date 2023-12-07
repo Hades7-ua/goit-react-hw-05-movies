33f720b190b865b3a213ba3c3a635ea565d2e9d4
Vitalii Tkachov <114616094+Hades7-ua@users.noreply.github.com>
2023-12-07 23:57:53 +0200

    Pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 // import { fetchTrendMovies,fetchSearchMovies,fetchMovieDetails } from "services/api";
-
+import CastPage from 'pages/CastPage';
+import HomePage from 'pages/HomePage';
+import MoviesDetailsPage from 'pages/MovieDetails';
+import MoviesPage from 'pages/MoviesPage';
+import NotFoundPage from 'pages/NotFoundPage';
+import ReviewsPage from 'pages/ReviewsPage';
+import { Routes, Route, NavLink } from 'react-router-dom';
 export const App = () => {
   // console.log(fetchTrendMovies())
   // console.log(fetchSearchMovies())
   // console.log(fetchMovieDetails())
-
 
   return (
     <>
@@ -18,8 +23,39 @@ export const App = () => {
           color: '#010101',
         }}
       >
-        hello
-       
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                {' '}
+                <NavLink to="/movies">Movies</NavLink>
+              </li>
+              <li>
+                <NavLink to="/movies/:movieId">Details</NavLink>
+              </li>
+              <li>
+                <NavLink to="cast">Cast</NavLink>
+              </li>
+              <li>
+                <NavLink to="reviews">Reviews</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/movies/:movieId" element={<MoviesDetailsPage />}>
+              <Route path="cast" element={<CastPage />} />
+              <Route path="reviews " element={<ReviewsPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
---
+++ src/pages/CastPage.js
@@ -0,0 +1,3 @@
+export default function CastPage() {
+  return <div>Cast</div>;
+}
