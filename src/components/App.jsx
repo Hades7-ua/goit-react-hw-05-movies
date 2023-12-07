9096b8b33f6ec2038af5cf46de5e3e59bf661330
Vitalii Tkachov <114616094+Hades7-ua@users.noreply.github.com>
2023-12-07 22:08:24 +0200

    fetch o_O

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+// import { fetchTrendMovies,fetchSearchMovies,fetchMovieDetails } from "services/api";
+
 export const App = () => {
+  // console.log(fetchTrendMovies())
+  // console.log(fetchSearchMovies())
+  // console.log(fetchMovieDetails())
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      hello
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        hello
+       
+      </div>
+    </>
   );
 };
---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+const API_KEY = '46a0cf4d73079c81a4257338e6778956';
+
+axios.defaults.baseURL = `https://api.themoviedb.org/3`;
+
+export const fetchTrendMovies = async () => {
+  const response = await axios.get(`/trending/movie/day?api_key=${API_KEY}`);
+  return response.data;
+};
+// console.log(fetchTrendMovies())
+export const fetchSearchMovies = async query => {
+  const response = await axios.get(
+    `/search/movie?query=${query}&api_key=${API_KEY}`
+  );
+  return response.data;
+};
+// console.log(fetchSearchMovies())
+
+// export const fetchMovieDetails = async movieId => {
+//   const response = await axios.get(`/movie/${movieId}?api_key=${API_KEY}`);
+//   return response.data;
+// };
